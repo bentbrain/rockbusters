2d336b56030c62d183e69fe19bb89c87514ccb36
liamkogan <103154727+liamkogan@users.noreply.github.com>
2023-01-02 18:16:45 +1100

    artsists added

---
+++ app/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const headerText = `Uh, I give you like a cryptic clue and some initials and it sort of makes up a band. So an easy one that we did at the start was, uh, an exploding pet, A.K – Atomic Kitten. Yeah, that's how it works.`;
+
+function Header() {
+  return <div>Header</div>;
+}
+
+export default Header;
