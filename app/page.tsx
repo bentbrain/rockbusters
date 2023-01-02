9597e473ebccaf4a30664cfef6839bdac1ea052b
liamkogan <103154727+liamkogan@users.noreply.github.com>
2023-01-02 12:34:41 +1100

    api route

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import sanityClient from "./client.js";
 
+const fetchURL = process.env.FETCH_URL;
+
+async function getData() {
+  const res = await fetch(`${fetchURL}api/question`);
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
 async function Home() {
+  const question = await getData();
+
   return (
     <div>
-      <h1>test</h1>
+      <h1>{question.hint}</h1>
     </div>
   );
 }
