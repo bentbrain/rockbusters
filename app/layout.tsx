4afcc0bb58e4ee7e4f9916540365c331f90d7dd8
liamkogan <103154727+liamkogan@users.noreply.github.com>
2023-01-03 16:36:49 +1100

    remove screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className="min-h-screen flex flex-col justify-start md:block">
+      <body className=" flex flex-col justify-start">
         <Header />
         <div className="wrapper p-4 grid items-center my-auto">
           <main className="max-w-[min(600px,100%)] my-6 bg-white p-4 rounded-md mx-auto shadow">
