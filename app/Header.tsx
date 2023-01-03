e24f8c3342d7ccdea4f75508a8bb0f08225e8906
liamkogan <103154727+liamkogan@users.noreply.github.com>
2023-01-03 14:44:26 +1100

    center header

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import InformationButton from "./InformationButton";
-
+import { IoInformationCircleOutline } from "react-icons/io5";
 const headerText = `Uh, I give you like a cryptic clue and some initials and it sort of makes up a band. So an easy one that we did at the start was, uh, an exploding pet, A.K – Atomic Kitten. Yeah, that's how it works.`;
 
 function Header() {
   return (
     <header className="bg-white p-4 shadow">
       <div className="inner max-w-[min(1200px,100%)] mx-auto text-center flex ">
+        <IoInformationCircleOutline
+          className="invisible"
+          opacity={0}
+          size={30}
+        />
         <div className="center -translate-x-5 flex gap-2 items-center mx-auto">
           <img className="w-10" src="/karl.png" alt="Karl Pilkington Head" />
           <h2 className="font-black text-xl text-stone-800">Rockbusters</h2>
