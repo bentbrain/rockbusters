206aca6f83c1a52bde1c2c2d57ff2a2daec24989
liamkogan <103154727+liamkogan@users.noreply.github.com>
2023-01-03 16:13:59 +1100

    min height remove

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,13 +7,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="bg-white">
+    <html lang="en" className="bg-stone-100">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className="bg-stone-100 min-h-screen">
+      <body className="">
         <Header />
         <div className="wrapper p-4">
           <main className="max-w-[min(600px,100%)] my-6 bg-white p-4 rounded-md mx-auto shadow">
