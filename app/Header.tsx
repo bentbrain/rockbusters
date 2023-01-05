f12b651f3359b0c68a6e470d3fc0fe01f5a1ce2e
liamkogan <103154727+liamkogan@users.noreply.github.com>
2023-01-05 22:34:07 +1100

    jamaica

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,7 +3,27 @@ import InformationButton from "./InformationButton";
 import { IoInformationCircleOutline } from "react-icons/io5";
 const headerText = `Uh, I give you like a cryptic clue and some initials and it sort of makes up a band. So an easy one that we did at the start was, uh, an exploding pet, A.K – Atomic Kitten. Yeah, that's how it works.`;
 
-function Header() {
+const fetchURL = process.env.FETCH_URL;
+
+async function getData() {
+  const res = await fetch(`${fetchURL}api/datequestion`, {
+    next: { revalidate: 60 },
+  });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+async function Header() {
+  const question = await getData();
+
   return (
     <header className="bg-white p-4 shadow">
       <div className="inner max-w-[min(1200px,100%)] mx-auto text-center flex ">
@@ -13,7 +33,15 @@ function Header() {
           size={30}
         />
         <div className="center -translate-x-5 flex gap-2 items-center mx-auto">
-          <img className="w-10" src="/karl.png" alt="Karl Pilkington Head" />
+          <img
+            className="w-10"
+            src={
+              question.hint.toLowerCase().includes("jamaican")
+                ? `/karl-jamaican.png`
+                : `/karl.png`
+            }
+            alt="Karl Pilkington Head"
+          />
           <h2 className="font-black text-xl text-stone-800">Rockbusters</h2>
         </div>
         <InformationButton />
--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
       */}
       <head />
       <body className=" flex flex-col justify-start">
+        {/* @ts-ignore */}
         <Header />
         <div className="wrapper p-4 grid items-center my-auto text-center">
           <main className="w-[min(600px,100%)] my-6 bg-white p-4 rounded-md mx-auto shadow">
