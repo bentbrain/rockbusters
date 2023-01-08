f051aeeaa6673638d1a4b60930e230735e6c09de
liamkogan <103154727+liamkogan@users.noreply.github.com>
2023-01-08 12:48:26 +1100

    scope change

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
-  scope: "/app",
 });
 
 /** @type {import('next').NextConfig} */
