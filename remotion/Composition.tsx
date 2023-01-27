ea8d89fd740f892064b989677c3cc5381ea28cbf
liamkogan <103154727+liamkogan@users.noreply.github.com>
2023-01-28 10:16:34 +1100

    remotion dark mode fix

--- remotion/Composition.tsx
+++ remotion/Composition.tsx
@@ -25,7 +25,7 @@ export const MyComposition = ({ text, audio }: Props) => {
   const scale = visualization.reduce((partialSum, a) => partialSum + a, 0);
 
   return (
-    <div className="w-full bg-white h-full flex flex-col justify-center gap-20  items-center text-center py-6 px-10 text-[5rem] font-bold ">
+    <div className="w-full bg-white dark:bg-stone-900 h-full flex flex-col justify-center gap-20  items-center text-center pt-6 pb-20 px-10 text-[5rem] font-bold ">
       <div>
         <img
           style={{
